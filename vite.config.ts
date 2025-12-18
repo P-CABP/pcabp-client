b05f92f54d076485a172c84bbe53cf377f816a71
codes <codesver@gmail.com>
2025-12-18 15:13:35 +0900

    chore: configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tsConfigPaths()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
