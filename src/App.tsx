2d973fec63e62c0cf003a0fa4008186273babf81
codes <codesver@gmail.com>
2025-12-22 22:33:16 +0900

    feat: compose query client helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import queryClient from "@/helpers/query-client";
+
 const App = () => {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <>App</>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
