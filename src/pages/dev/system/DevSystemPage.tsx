73e4fbdb3b7e0b7ceca1d3ffd6fe9388576e07f9
codes <codesver@gmail.com>
2026-01-03 15:37:52 +0900

    feat: compose system dev page

--- src/pages/dev/system/DevSystemPage.tsx
+++ src/pages/dev/system/DevSystemPage.tsx
@@ -1,14 +1,37 @@
 import { Button, Stack } from "@mui/material";
 
+import {
+  SystemModalMessageType,
+  useAlert,
+  useConfirm,
+} from "@/stores/system-modal-store";
 import { SystemToastMessageType, useToast } from "@/stores/system-toast-store";
 
 const DevSystemPage = () => {
   const openToast = useToast();
+  const confirm = useConfirm();
+  const alert = useAlert();
 
   const handleToastOpen = (type: SystemToastMessageType) => {
     openToast({
       type,
       message: `Toast ${type}`,
+    });
+  };
+
+  const handleConfirmOpen = () => {
+    confirm({
+      message: "Confirm",
+      onConfirm: () => {
+        console.log("Confirm");
+      },
+    });
+  };
+
+  const handleAlertOpen = (type: SystemModalMessageType) => {
+    alert({
+      type,
+      message: `Alert ${type}`,
     });
   };
 
@@ -22,6 +45,18 @@ const DevSystemPage = () => {
       </Button>
       <Button variant="contained" onClick={() => handleToastOpen("error")}>
         Toast Error
+      </Button>
+      <Button variant="contained" onClick={handleConfirmOpen}>
+        Modal Confirm
+      </Button>
+      <Button variant="contained" onClick={() => handleAlertOpen("info")}>
+        Modal Info
+      </Button>
+      <Button variant="contained" onClick={() => handleAlertOpen("success")}>
+        Modal Success
+      </Button>
+      <Button variant="contained" onClick={() => handleAlertOpen("error")}>
+        Modal Error
       </Button>
     </Stack>
   );
